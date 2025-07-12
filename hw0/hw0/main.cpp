6dc4b25c2931767eb05d360a80ae1e6343337315
radcli14 <radcli14@gmail.com>
2025-07-12 10:12:27 +0200

    originals for homework 0

---
+++ hw0/hw0/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw0
+//
+//  Created by Eliott Radcliffe on 7/11/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
