9ee92d523ecae87350f83605b27f4121e69b9f51
radcli14 <radcli14@gmail.com>
2025-07-25 10:26:44 +0200

    brought in the transform code from hw1

--- hw2-linux_osx/Transform.cpp
+++ hw2-linux_osx/Transform.cpp
@@ -6,33 +6,71 @@
 
 #include "Transform.h"
 
+mat3 identity3 = mat3(1.0f); // 3x3 identity matrix
+
 // Helper rotation function.  Please implement this.  
 mat3 Transform::rotate(const float degrees, const vec3& axis) 
 {
-  mat3 ret;
-  // YOUR CODE FOR HW2 HERE
-  // Please implement this.  Likely the same as in HW 1.  
-  return ret;
+  // Preload cosine and sine functions
+	float c = cos(degrees * pi / 180.0f);
+	float s = sin(degrees * pi / 180.0f);
+
+	// Outer product of the axis with itself
+	mat3 outer = mat3(
+		axis.x * axis.x, axis.x * axis.y, axis.x * axis.z,
+		axis.y * axis.x, axis.y * axis.y, axis.y * axis.z,
+		axis.z * axis.x, axis.z * axis.y, axis.z * axis.z
+	);
+
+	// Cross product matrix
+	mat3 aprime = transpose(mat3(
+		0.0f, -axis.z, axis.y,
+		axis.z, 0.0f, -axis.x,
+		-axis.y, axis.x, 0.0f
+	));
+
+	// Rodrigues Rotation Formula
+	return c * identity3 + (1.0f - c) * outer + s * aprime;
 }
 
 void Transform::left(float degrees, vec3& eye, vec3& up) 
 {
-  // YOUR CODE FOR HW2 HERE
-  // Likely the same as in HW 1.  
+  // Obtain the rotation matrix, then update the eye and up vectors 
+	// according to a rotation of degrees aboout the up vector
+	mat3 R = rotate(degrees, up);
+	eye = R * eye;
+	up = R * up;
 }
 
 void Transform::up(float degrees, vec3& eye, vec3& up) 
 {
-  // YOUR CODE FOR HW2 HERE 
-  // Likely the same as in HW 1.  
+	// Obtain the axis pointing to the right of the eye using a cross product of eye and up
+	vec3 right = normalize(cross(eye, up));
+
+	// Obtain the rotation matrix, then update the eye and up vectors 
+	// according to a rotation of degrees aboout the right vector
+	mat3 R = rotate(degrees, right);
+	eye = R * eye;
+	up = R * up;
 }
 
 mat4 Transform::lookAt(const vec3 &eye, const vec3 &center, const vec3 &up) 
 {
-  mat4 ret;
-  // YOUR CODE FOR HW2 HERE
-  // Likely the same as in HW 1.  
-  return ret;
+	// Compute the u, v, w vectors according to the equations provided in the 
+	// orthornormal coordinate frames lecture, where a is eye, b is up
+	vec3 u, v, w;
+	w = normalize(eye);
+	u = normalize(cross(up, w));
+	v = cross(w, u);
+
+	// Form the rotation matrix using the u, v, w vector, and assume e is the eye,
+	// apply these to the gluLookAt final form equation
+	return transpose(mat4(
+		u.x, u.y, u.z, -(u.x * eye.x + u.y * eye.y + u.z * eye.z),
+		v.x, v.y, v.z, -(v.x * eye.x + v.y * eye.y + v.z * eye.z),
+		w.x, w.y, w.z, -(w.x * eye.x + w.y * eye.y + w.z * eye.z),
+		0.0f, 0.0f, 0.0f, 1.0f
+	));
 }
 
 mat4 Transform::perspective(float fovy, float aspect, float zNear, float zFar)
